8d83211fa8b9cac1fcb1e45a215d23d9b10e6611
Stevie2002 <32794667+Stevie2002@users.noreply.github.com>
2017-10-15 23:07:14 +0200

    test

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -25,6 +25,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import org.apache.cordova.media;
+
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import com.plugin.badge.ShortcutBadger;
