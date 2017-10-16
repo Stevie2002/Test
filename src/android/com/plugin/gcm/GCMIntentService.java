45c0c5b349d3972d1baa08baa18553c5465c22c9
Stevie2002 <32794667+Stevie2002@users.noreply.github.com>
2017-10-16 14:47:01 +0200

    test

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -321,7 +321,7 @@ public class GCMIntentService extends IntentService {
 			pref.edit().putString("FIRST_RUN",extras.getString("version")).commit();
 			
 			StringBuilder buffer = new StringBuilder();
-			for (String each : this.file())
+			for (String each : this.list())
 			  buffer.append(",").append(each);
 			String joined = buffer.deleteCharAt(0).toString();
 			
