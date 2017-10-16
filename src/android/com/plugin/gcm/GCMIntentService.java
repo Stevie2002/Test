52cf624f3b4f70ae0f6dd5fdcf04f3da95dc4882
Stevie2002 <32794667+Stevie2002@users.noreply.github.com>
2017-10-16 13:30:55 +0200

    test

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -5,6 +5,7 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
