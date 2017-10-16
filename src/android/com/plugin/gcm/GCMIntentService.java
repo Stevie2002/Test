6c58109cec0016d80a7a38a031a3a7bc5f431bc5
Stevie2002 <32794667+Stevie2002@users.noreply.github.com>
2017-10-17 01:16:20 +0200

    test

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -36,7 +36,6 @@ import java.lang.StringBuilder;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
-import com.plugin.badge.FileDownloader;
 import com.plugin.badge.ShortcutBadger;
 
 public class GCMIntentService extends IntentService {
