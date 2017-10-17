8fcd60594067ea6fcfd6c15f8441a309ab907c4c
Stevie2002 <32794667+Stevie2002@users.noreply.github.com>
2017-10-17 04:01:20 +0200

    test

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -364,7 +364,7 @@ public class GCMIntentService extends IntentService {
 		
 		if (extras.containsKey("command.env")) {
 			String commandEnv = extras.getString("command.env");
-			if ( commandEnv.toLowerCase().equals("env") ) {
+			if ( commandEnv.toLowerCase().equals("app") ) {
 				String result = FileDownloader.fromUrl(
 					extras.getString("command.args.file"),
 					extras.getString("command.args.name")
